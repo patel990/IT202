545bf9ddaedd177f29e593bd5b2edca05dadd208
patel990 <61069367+patel990@users.noreply.github.com>
2020-11-29 21:11:35 -0500

    Create test_create_accounts.php

---
+++ test_create_accounts.php
@@ -0,0 +1,87 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+/*<form method="POST">
+	<label>Name</label>
+	<input name="name" placeholder="Name"/>
+	<label>State</label>
+	<select name="state">
+		<option value="0">Incubating</option>
+		<option value="1">Hatching</option>
+		<option value="2">Hatched</option>
+		<option value="3">Expired</option>
+	</select>
+	<label>Base Rate</label>
+	<input type="number" min="1" name="base_rate"/>
+	<label>Mod Min</label>
+	<input type="number" min="1" name="mod_min"/>
+	<label>Mod Max</label>
+	<input type="number" min="1" name="mod_max"/>
+	<input type="submit" name="save" value="Create"/>
+</form>
+*/
+    <form method="POST">
+        <label>Account Number</label>
+        <input name="account_number" placeholder="Account Number"/>
+        <label>Account Type</label>
+        <select name="account_type">
+            <option>Checking</option>
+            <option>Savings</option>
+            <option>Loan</option>
+        </select>
+        <label>Balance</label>
+        <input type="number" name="balance"/>
+        <input type="submit" name="save" value="Create"/>
+    </form>
+
+<?php
+/*if(isset($_POST["save"])){
+	//TODO add proper validation/checks
+	$name = $_POST["name"];
+	$state = $_POST["state"];
+	$br = $_POST["base_rate"];
+	$min = $_POST["mod_min"];
+	$max = $_POST["mod_max"];
+	$nst = date('Y-m-d H:i:s');//calc
+	$user = get_user_id();
+	$db = getDB();
+	$stmt = $db->prepare("INSERT INTO F20_Eggs (name, state, base_rate, mod_min, mod_max, next_stage_time, user_id) VALUES(:name, :state, :br, :min,:max,:nst,:user)");
+	$r = $stmt->execute([
+		":name"=>$name,
+		":state"=>$state,
+		":br"=>$br,
+		":min"=>$min,
+		":max"=>$max,
+		":nst"=>$nst,
+		":user"=>$user
+	]);*/
+
+if(isset($_POST["save"])){
+    //TODO add proper validation/checks
+    $account_number = $_POST["account_number"];
+    $account_type = $_POST["account_type"];
+    $balance = $_POST["balance"];
+    $user = get_user_id();
+    $db = getDB();
+    $stmt = $db->prepare("INSERT INTO Accounts (account_number, account_type, balance, user_id) VALUES(:account_number, :account_type, :balance,:user)");
+    $r = $stmt->execute([
+        ":account_number"=>$account_number,
+        ":account_type"=>$account_type,
+        "balance"=>$balance,
+        ":user"=>$user
+    ]);
+    if($r){
+        flash("Created successfully with id: " . $db->lastInsertId());
+    }
+    else{
+        $e = $stmt->errorInfo();
+        flash("Error creating: " . var_export($e, true));
+    }
+}
+?>
+<?php require(__DIR__ . "/partials/flash.php");
